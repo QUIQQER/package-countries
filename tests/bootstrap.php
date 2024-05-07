a7f4737661db31a345684fac852bcd8f2850b320
Henning Leutz <leutz@pcsg.de>
2024-05-07 15:09:11 +0200

    fix: quiqqer/quiqqer locale and paths adapted to quiqqer/core

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,4 +7,4 @@ $quiqqerPackageDir = dirname(dirname(__FILE__));
 $packageDir = dirname(dirname($quiqqerPackageDir));
 
 // include quiqqer bootstrap for tests
-require $packageDir . '/quiqqer/quiqqer/tests/bootstrap.php';
+require $packageDir . '/quiqqer/core/tests/bootstrap.php';
